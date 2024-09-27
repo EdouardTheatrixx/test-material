b808b7e9d841bb0a8207a12d0b4fda04265cda2d
EdouardTheatrixx <esalembier@theatrixx.com>
2024-09-27 16:01:43 -0400

    btn test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  constructor() {
+    const element = document.body;
+    element.classList.add('theme-light');
+  }
   title = 'test';
+  array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  toggleTheme() {
+    const element = document.body;
+    element.classList.toggle('theme-dark');
+    element.classList.toggle('theme-light');
+  }
 }
